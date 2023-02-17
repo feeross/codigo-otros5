7ad9b58feaaf8c4a0eeb9378fc88698e8d3d8363
Fernanda Rosales <fernanda.rosales.ortega@gmail.com>
2023-02-17 16:26:12 -0600

    revision y correcion de codigo de otros

--- Codigo5.java
+++ Codigo5.java
@@ -1,29 +1,36 @@
-public class Codigo5 {
+package com.generation;
 
-	    Scanner s = new Scanner();
-	    System.out.print("Introduzca un número: ');
+import java.util.Scanner;// se importo java util
+
+public class CodigoOtros5 {
+	
+	public static void name() { //agregar static metodo
+		Scanner s = new Scanner(System.in); //se agrego system.in
+	    System.out.println("Introduzca un número: ");//cambiar comilla
 	    String ni = s.nextLine();
-	    int c = ni;
+	    int c = Integer.parseInt(ni);//se convirtio el String en numerber
 	    
 	    int afo = 0;
 	    int noAfo = 0;
 	    
-	    while (ni > 0) {
-		  int digito = (int)(ni % 10);
+	    while (c > 0) { //se cambio la variable a c ya que se requiere un entero
+		  int digito = (ni % 10);
 	      if ((digito == 3) || (digito == 7) || (digito == 8) || (digito == 9)) {
 			afo++;
 	      } else {
 			noAfo++;
 		  
-		  ni /= 10;
+		  //int ni /= 10;
 	    }
 
-	    if (afo > noAfo) {
+	    if (c > noAfo) { // se tiene que comparar un entero
 	      System.out.prinln("El " + c + " es un número afortunado.");
 	    } else {
 	      System.out.println("El " + c + " no es un número afortunado.");
 	    }
 	    
 	  }
-	  
+		
 	}
+
+}
